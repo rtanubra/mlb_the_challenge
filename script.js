3db38dab0466c469fef7d4e6d850054619018cf5
rtanubra <rdtanubrata@gmail.com>
2019-03-31 18:51:19 -0400

    Min Viable Product- able to compare two players in the stats of interest

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const bestStatsHitter = {
     "SB":45,
     "AVG":0.346,
     "CS":0.01,
+    "G":162
 }
 
 function createUrl(params,baseUrl){
@@ -93,9 +94,82 @@ function fetchBasic(){
         err=> console.log(err)
     )
 }
+function updateDualTrial(data){
+    /**
+     * {
+        "players":players,
+        "stats":stats,
+        "statsNames":statsNames,
+        "statsAbrvs":statsAbrvs
+        }   
+     */
+    
+    //just a little unpacking for ease of use
+    const players = data["players"]
+    const stats = data["stats"]
+    const statsAbrvs = data["statsAbrvs"]
+    const statsNames = data["statsNames"]
 
-function gatherDualBasic(myJson){
+    $(".player1_stats").empty()
+    $(".player2_stats").empty()
 
+    //update titles
+    $(".player1-header").text(`${data["players"][0]}`)
+    $(".player2-header").text(`${data["players"][1]}`)
+
+    for (let i=0; i<statsAbrvs.length ; i++){
+        $(".player1_stats").append(`
+        <tr>
+            <td>${statsNames[i].slice(0,15)} (${statsAbrvs[i]})</td>
+            <td>${stats[statsAbrvs[i]][0]}</td>
+            <td>${Math.round(stats[statsAbrvs[i]][0]/bestStatsHitter[statsAbrvs[i]]*10000)/100}</td>
+        </tr>
+        `)
+        $(".player2_stats").append(`
+        <tr>
+            <td>${statsNames[i].slice(0,15)} (${statsAbrvs[i]})</td>
+            <td>${stats[statsAbrvs[i]][1]}</td>
+            <td>${Math.round(stats[statsAbrvs[i]][1]/bestStatsHitter[statsAbrvs[i]]*10000)/100}</td>
+        </tr>
+        `)
+    }
+    
+}
+function gatherDualTrial(myJson){
+    console.log(myJson)
+    const playerStats = myJson['cumulativeplayerstats']["playerstatsentry"]
+    const statsAbrvHigh = ["R","HR","RBI","AVG","SB"]
+    const statsAbrvLow = ["CS"]
+    
+    const statsAbrvs = []
+    const statsNames = Object.keys(playerStats[0]["stats"])
+    for (let i=0; i< statsNames.length;i++){
+        statsAbrvs.push(playerStats[0]["stats"][statsNames[i]]["@abbreviation"])
+    }
+    const players = [
+        `${playerStats[0]["player"]["FirstName"]} ${playerStats[0]["player"]["LastName"]} (${playerStats[0]["player"]["JerseyNumber"]})`,
+        `${playerStats[1]["player"]["FirstName"]} ${playerStats[1]["player"]["LastName"]} (${playerStats[1]["player"]["JerseyNumber"]})`
+    ]
+    let stats = {
+        "G":[0,0],
+        "R":[0,0],
+        "HR":[0,0],
+        "RBI":[0,0],
+        "AVG":[0,0],
+        "SB":[0,0],
+        "CS":[0,0],
+    }
+    for (let i =0; i<2; i++){
+        for (let x = 0; x<statsAbrvs.length; x++){
+            stats[statsAbrvs[x]][i] = parseFloat(playerStats[i]["stats"][statsNames[x]]["#text"])
+        }
+    }
+    return {
+        "players":players,
+        "stats":stats,
+        "statsNames":statsNames,
+        "statsAbrvs":statsAbrvs
+    }
 }
 
 function fetchDualTrial(){
@@ -104,7 +178,7 @@ function fetchDualTrial(){
             "Authorization": `Basic ${btoa(`${apikey}:${password}`)})`
         })
     }
-    const baseUrl= "https://api.mysportsfeeds.com/v1.0/pull/mlb/2019-regular/player_gamelogs.json"
+    const baseUrl= "https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/cumulative_player_stats.json"
     const params = {
         "player":"Mookie-Betts,Jose-Altuve",
         "playerstats":"r,hr,rbi,avg,sb,cs",
@@ -122,7 +196,8 @@ function fetchDualTrial(){
         }
     ).then(
         responseJson=>{
-            gatherDualBasic(responseJson)
+            const data = gatherDualTrial(responseJson)
+            updateDualTrial(data)
         }
     ).catch(
         err=> console.log(err)
