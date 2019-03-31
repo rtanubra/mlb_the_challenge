c22e325da69100d605cc7a6d61c4ec0fd2053000
rtanubra <rdtanubrata@gmail.com>
2019-03-31 18:03:24 -0400

    working on basic fetch of mysportsfeeds

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 "use strict";
 const apikey = "e09cc51e-beb1-43ac-b888-20a591";
 const password = "reytapptester110";
+const bestStatsHitter = {
+    "R":129,
+    "HR":48,
+    "RBI":130,
+    "SB":45,
+    "AVG":0.346,
+    "CS":0.01,
+}
 
 function createUrl(params,baseUrl){
     let final_url = baseUrl+"?"
@@ -12,7 +20,47 @@ function createUrl(params,baseUrl){
 }
 
 function gatherBasic(myJson){
-    console.log(myJson)
+    //console.log(myJson)
+    const gamelogs = myJson['playergamelogs']["gamelogs"]
+    const statsAbrvHigh = ["R","HR","RBI","AVG","SB"]
+    const statsAbrvLow = ["CS"]
+    let stats = {
+        "R":[],
+        "HR":[],
+        "RBI":[],
+        "AVG":[],
+        "SB":[],
+        "CS":[],
+    }
+    let statsBest = {
+        "R":0,
+        "HR":0,
+        "RBI":0,
+        "AVG":0,
+        "SB":0,
+        "CS":0,
+    }
+    const statNames = Object.keys(gamelogs[0]["stats"]) 
+    //console.log(statNames)
+
+    //Fill statistics
+    for (let i = 0; i< gamelogs.length;i++){
+        for (let j = 0; j< statNames.length; j++){
+            stats[gamelogs[i]["stats"][statNames[j]]["@abbreviation"]].push(parseFloat(gamelogs[i]["stats"][statNames[j]]["#text"]))
+        }
+    }
+
+    //console.log(stats)
+    for (let i =0; i<statsAbrvHigh.length;i++){
+        statsBest[statsAbrvHigh[i]] = Math.max(...stats[statsAbrvHigh[i]])
+    }
+    for (let i =0; i<statsAbrvLow.length;i++){
+        statsBest[statsAbrvLow[i]] = Math.min(...stats[statsAbrvLow[i]])
+    }
+
+    console.log(statsBest)
+    console.log(stats)
+
 }
 
 function fetchBasic(){
@@ -23,8 +71,8 @@ function fetchBasic(){
     }
     const baseUrl= "https://api.mysportsfeeds.com/v1.0/pull/mlb/2018-regular/player_gamelogs.json"
     const params = {
-        "player":"Mike-Trout,Mookie-Betts,Nolan-Arenado,Jose-Altuve,Alex-Bregman",
-        "playerstats":"ab,r,h,bb",
+        "player":"Mookie-Betts,Jose-Altuve,Mike-Trout",
+        "playerstats":"r,hr,rbi,avg,sb,cs",
         "sort":"player.lastname"
     }
     const fetchUrl = createUrl(params,baseUrl)
@@ -46,11 +94,47 @@ function fetchBasic(){
     )
 }
 
+function gatherDualBasic(myJson){
+
+}
+
+function fetchDualTrial(){
+    const options= {
+        headers: new Headers({
+            "Authorization": `Basic ${btoa(`${apikey}:${password}`)})`
+        })
+    }
+    const baseUrl= "https://api.mysportsfeeds.com/v1.0/pull/mlb/2019-regular/player_gamelogs.json"
+    const params = {
+        "player":"Mookie-Betts,Jose-Altuve",
+        "playerstats":"r,hr,rbi,avg,sb,cs",
+        "sort":"player.lastname"
+    }
+    const fetchUrl = createUrl(params,baseUrl)
+    fetch(fetchUrl,options).then(
+        response =>{
+            if (response.ok){
+                return response.json()
+            }
+            else{
+                throw new Error(response.statusText)
+            }
+        }
+    ).then(
+        responseJson=>{
+            gatherDualBasic(responseJson)
+        }
+    ).catch(
+        err=> console.log(err)
+    )
+}
+
 function watchClickMe(){
     console.log("Ready to receive a click me.")
     $(".my-button").click(function(){
         console.log("clicked")
-        fetchBasic()
+        //fetchBasic()
+        fetchDualTrial()
     })
 }
 
